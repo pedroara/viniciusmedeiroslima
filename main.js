3b2eb78fe49f13c099da4aa759126be23038da3f
pedroara <araujo.peo@gmail.com>
2020-11-03 20:53:27 -0300

    v2

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
-
+//Express
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/ping", function (req, res) {
