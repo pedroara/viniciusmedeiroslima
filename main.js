379832573700c90434d4b57fb4e34392ed8f1fed
pedroara <araujo.peo@gmail.com>
2020-11-03 21:01:20 -0300

    teste 3

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/* const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
@@ -12,3 +12,13 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 app.listen(port);
+ */
+var http = require("http");
+const express = require("express");
+const app = express();
+
+const port = porta_da_aplicação;
+app.listen(port);
+const baseDir = `${__dirname}/build/`;
+app.use(express.static(`${baseDir}`));
+app.get("/", (req, res) => res.sendfile("index.html", { root: baseDir }));
